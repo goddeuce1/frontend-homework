dd9196346e7af1caa82e037628431181bb0d9f58
Dmitriy Zaharov <goddeuce1@gmail.com>
2019-02-05 19:22:25 +0300

    Добавлены тесты

--- tests/get.js
+++ tests/get.js
@@ -17,6 +17,11 @@ QUnit.module('Тестируем функцию get', function () {
 		assert.deepEqual(get(object, '.deep.hested'), object.deep.hested);
 		assert.deepEqual(get(object, '.deep'), object.deep);
 		assert.deepEqual(get(object, '.'), object);
+		
+		assert.strictEqual(get(object, '.foo.length'), object.foo.length);
+		assert.strictEqual(get(object, '.deep.hested.field.length'), object.deep.hested.field.length);
+		
+
 	});
 
 	QUnit.test('get работает правильно c массивами', function (assert) {
@@ -33,6 +38,10 @@ QUnit.module('Тестируем функцию get', function () {
 		assert.strictEqual(get(object, '.baz.0'), object.baz[ 0 ]);
 		assert.strictEqual(get(object, '.baz.length'), object.baz.length);
 		assert.strictEqual(get(object, '.deep.0.foobar'), object.deep[ 0 ].foobar);
+		
+		assert.strictEqual(get(object, '.deep.0.foobar.length'), object.deep[0].foobar.length);
+		assert.strictEqual(get(object, '.foo.2'), object.foo[2]);
+
 	});
 
 	QUnit.test('get работает правильно c объектами без свойств', function (assert) {
@@ -47,5 +56,24 @@ QUnit.module('Тестируем функцию get', function () {
 		assert.strictEqual(get(object, '.baz.0'), undefined);
 		assert.strictEqual(get(object, '.baz.length'), undefined);
 		assert.strictEqual(get(object, '.0.1.2'), undefined);
+		
+		assert.strictEqual(get(object, '.0.test.length.length'), undefined);
+		assert.strictEqual(get(object, '.length.1.0.length'), undefined);
+	
+		
 	});
+	
+	QUnit.test('get работает правильно c length', function (assert) {
+		const object = {
+    		foo: {
+        		bar: 42
+    		},
+    		length: "test"
+		};
+
+		assert.strictEqual(get(object, '.length'), object.length);
+		assert.strictEqual(get(object, '.length.length'), object.length.length);
+
+	});
+	
 });
