6323c98a2b2740b82dd42a0c215c261ace376681
Dmitriy Zaharov <goddeuce1@gmail.com>
2019-02-06 06:07:07 +0300

    fixes (have read about => & hasOwnProperty)

--- source/get.js
+++ source/get.js
@@ -1,19 +1,19 @@
 "use strict";
 
-const get = function(object, string) {
-    if (!string.localeCompare(".")) {
-        return object;
+let get = (object, string) => {
+    if (!string.localeCompare("")) {
+        return;
     }
     const stringArray = string.split('.');
     for (let i = 1; i < stringArray.length; ++i) {
-        if (object[stringArray[i]] === undefined) {
-            return undefined;
-        } else if (!isNaN(stringArray[i])) {
-            object = object[parseInt(stringArray[i], 10)];
-        } else if (!stringArray[i].localeCompare("length") && i == stringArray.length - 1) {
-            return object.length;
+        if (stringArray[i].localeCompare("")) {
+            if (object.hasOwnProperty(stringArray[i])) {
+                object = object[stringArray[i]];
+            } else {
+                return;
+            }
         } else {
-            object = object[stringArray[i]];
+            return object;
         }
     }
     return object;
