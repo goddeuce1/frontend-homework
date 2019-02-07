4a12dc11ee953d68b99903eb5515b341e17259d6
Dmitriy Zaharov <goddeuce1@gmail.com>
2019-02-07 18:58:03 +0300

    style/default params

--- source/get.js
+++ source/get.js
@@ -1,20 +1,25 @@
 "use strict";
 
-let get = (object, string) => {
+const get = (object = undefined, string = ".") => {
+
     if (!string.localeCompare("")) {
         return;
     }
+
     const stringArray = string.split('.');
+
     for (let i = 1; i < stringArray.length; ++i) {
-        if (stringArray[i].localeCompare("")) {
-            if (object.hasOwnProperty(stringArray[i])) {
-                object = object[stringArray[i]];
-            } else {
-                return;
-            }
-        } else {
+
+        if (!(stringArray[i].localeCompare(""))) {
             return object;
+        }
+
+        if (!(object.hasOwnProperty(stringArray[i]))) {
+            return;
+        } else {
+            object = object[stringArray[i]];
         }
     }
     return object;
 }
+
